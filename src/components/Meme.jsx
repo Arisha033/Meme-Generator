b92201576f5288db81cc6e842374cf9ecfa2ce79
Arisha <arishaaa333@gmail.com>
2023-11-27 22:42:08 +0530

    changes in state

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,32 +1,39 @@
 import React, { useState } from "react";
-import MemesData from '../MemesData.jsx'
-
+import MemesData from "../MemesData.jsx";
 
 export const Meme = () => {
+  // const [memeImage, setmemeImage] = useState("")
 
-  let imgUrl;
-const [memeImage, setmemeImage] = useState("")
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
+  const [allMemeImage, setAllMemeImage] = useState(MemesData);
 
-function getMemeImage(){
-  const memeArray = MemesData.data.memes
-  const randomNumber = Math.floor(Math.random() * memeArray.length)
-   imgUrl = memeArray[randomNumber].url;
-   setmemeImage(imgUrl);
-  // console.log(imgUrl)
-}
-  
+  function getMemeImage() {
+    const memeArray = allMemeImage.data.memes;
+    const randomNumber = Math.floor(Math.random() * memeArray.length);
+     const imgUrl = memeArray[randomNumber].url;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: imgUrl
+    }));
+    // console.log(imgUrl)
+  }
+
   return (
     <>
       <div className="form">
-        <input type="text" className="input-field" placeholder="Top Text"/>
-        <input type="text" className="input-field" placeholder="Bottom Text "/>
+        <input type="text" className="input-field" placeholder="Top Text" />
+        <input type="text" className="input-field" placeholder="Bottom Text " />
       </div>
 
       <div className="form-btn">
         <button className="btn" onClick={getMemeImage}>
           Get a new meme image🖼️
         </button>
-        <img src={memeImage} alt="memeImage"  className="memeImage"/>
+        <img src={meme.randomImage} alt="memeImage" className="memeImage" />
       </div>
     </>
   );
