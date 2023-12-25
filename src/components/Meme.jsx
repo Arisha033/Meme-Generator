1c6104dd5e3b290141236645799761be6894d535
Arisha <arishaaa333@gmail.com>
2023-12-25 23:46:44 +0530

    handle form input

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -9,15 +9,26 @@ export const Meme = () => {
     bottomText: "",
     randomImage: "http://i.imgflip.com/1bij.jpg",
   });
+
+  function handleInputChange(event) {
+    const { name, value } = event.target;
+    setMeme((prevMemeData) => {
+      return {
+        ...prevMemeData,
+        [name]: value,
+      };
+    });
+  }
+
   const [allMemeImage, setAllMemeImage] = useState(MemesData);
 
   function getMemeImage() {
     const memeArray = allMemeImage.data.memes;
     const randomNumber = Math.floor(Math.random() * memeArray.length);
-     const imgUrl = memeArray[randomNumber].url;
+    const imgUrl = memeArray[randomNumber].url;
     setMeme((prevMeme) => ({
       ...prevMeme,
-      randomImage: imgUrl
+      randomImage: imgUrl,
     }));
     // console.log(imgUrl)
   }
@@ -25,15 +36,33 @@ export const Meme = () => {
   return (
     <>
       <div className="form">
-        <input type="text" className="input-field" placeholder="Top Text" />
-        <input type="text" className="input-field" placeholder="Bottom Text " />
+        <input
+          type="text"
+          className="input-field"
+          placeholder="Top Text"
+          name="topText"
+          value={meme.topText}
+          onChange={handleInputChange}
+        />
+        <input
+          type="text"
+          className="input-field"
+          placeholder="Bottom Text"
+          name="bottomText"
+          value={meme.bottomText}
+          onChange={handleInputChange}
+        />
       </div>
 
       <div className="form-btn">
         <button className="btn" onClick={getMemeImage}>
           Get a new meme image🖼️
         </button>
+      <div className="meme">
         <img src={meme.randomImage} alt="memeImage" className="memeImage" />
+        <h2 className="memeText top">{meme.topText}</h2>
+        <h2 className="memeText bottom">{meme.bottomText}</h2>
+      </div>
       </div>
     </>
   );
